cf6fe92899bcefbb22c5754dcbd32654feff879c
Marcin Przybył <marcin.przybyl.91@gmail.com>
2024-10-19 08:27:27 +0200

    Init

---
+++ lua/plugins/coding.lua
@@ -0,0 +1,7 @@
+return {
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.lang.angular" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
+}
